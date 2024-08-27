fdb57281fe3f53e9125021c6e488106bd33e863b
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2024-08-27 14:49:13 +0800

    Update theme to catppuccin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import defaultTheme from "tailwindcss/defaultTheme";
 import formsPlugin from "@tailwindcss/forms";
+import catppuccinPlugin from "@catppuccin/tailwindcss";
 import type { Config } from "tailwindcss";
+import type { PluginCreator, PluginsConfig } from "tailwindcss/types/config";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -11,5 +13,14 @@ export default {
       },
     },
   },
-  plugins: [formsPlugin],
+  plugins: [
+    formsPlugin,
+    catppuccinPlugin({
+      // prefix to use, e.g. `text-pink` becomes `text-ctp-pink`.
+      // default is `false`, which means no prefix
+      prefix: "ctp",
+      // which flavour of colours to use by default, in the `:root`
+      defaultFlavour: "mocha",
+    }) as PluginsConfig[number],
+  ],
 } satisfies Config;
